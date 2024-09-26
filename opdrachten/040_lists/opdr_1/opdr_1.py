137d02480ceca67875c555d5c7630e082e5e1bfb
97078405 <97078405@st.deltion.nl>
2024-09-26 15:08:29 +0200

    .

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,16 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+
+dict_1 = {"id": 0, "voornaam": "Jan", "achternaam": "Jansen"}
+dict_2 = {"id": 1, "voornaam": "Eva", "achternaam": "De Vries"}
+dict_3 = {"id": 2, "voornaam": "Tom", "achternaam": "Bakker"}
+dict_4 = {"id": 3, "voornaam": "Lisa", "achternaam": "Pieters"}
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+print(mylist[2]["voornaam"], mylist[2]["achternaam"])
