1b2cd2da32831f95cfb103a5bbc4bf0e4ea0b065
97078405 <97078405@st.deltion.nl>
2024-09-26 15:16:56 +0200

    .

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -2,6 +2,14 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+
+import math
+
+zijde_1 = float(input("Geef de lengte van de eerste zijde\n"))
+zijde_2 = float(input("Geef de lengte van de tweede zijde\n"))
+
+schuine_zijde = math.sqrt(zijde_1**2 + zijde_2**2)
+
+print(f"\nDe lengte van de schuine zijde is: {schuine_zijde}")
 
 
--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,13 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+
+gasten = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+print("Lijst met gasten:", gasten)
+
+gasten.remove("Marie")
+print("Lijst na annulering van Marie:", gasten)
+
+index_van_kees = gasten.index("Kees") 
+gasten.insert(index_van_kees + 1, "George") 
+print("Lijst na toevoeging van George:", gasten)
