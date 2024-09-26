243a49a943d927c799dc272c4e51e0a127be86df
97078405 <97078405@st.deltion.nl>
2024-09-26 14:33:30 +0200

    .

--- opdrachten/030_berekeningen/opdr_3/opdr_3.py
+++ opdrachten/030_berekeningen/opdr_3/opdr_3.py
@@ -3,14 +3,4 @@
 # Groep:
 
 # Hier komt je code...
-x = 1
-y = ...
-print( ... )
-
-x = 2
-y = ...
-print( ... )
-
-x = 0
-y = ...
-print( ... )
+# Functie om de waarde van y te berekenen voor een gegeven x
